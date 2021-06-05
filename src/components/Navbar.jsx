effb9e334ad4d1597374ad6aef09bc4624b0a375
Anandhan <anandhanedom@gmail.com>
2021-06-06 04:49:57 +0530

    apply transition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { Link } from "gatsby";
 
+const navLinks = [
+  {
+    title: "Services",
+    endPoint: "/services",
+  },
+  {
+    title: "About",
+    endPoint: "/about",
+  },
+  {
+    title: "Contact",
+    endPoint: "/contact",
+  },
+];
+
 const Navbar = () => {
   return (
     <nav className="bg-transparent">
       <div className="container px-6 sm:pt-16 md:px-12 xl:px-0 max-w-7xl xl:mx-auto md:flex md:justify-between md:items-center">
         <div className="flex items-center justify-between">
           <div>
-            <a
-              className="text-2xl font-bold text-white lg:text-3xl hover:text-gray-700"
-              href="#"
+            <Link
+              className="text-2xl font-bold text-white lg:text-3xl"
+              href="/"
             >
               vonnue
-            </a>
+            </Link>
           </div>
 
           <div className="flex md:hidden">
@@ -33,30 +48,14 @@ const Navbar = () => {
 
         <div className="items-center md:flex">
           <div className="flex flex-col md:flex-row md:mx-6">
-            <Link
-              className="my-1 text-sm font-medium text-white sm:text-xl sm:font-medium md:mx-4 md:my-0"
-              to="#"
-            >
-              Home
-            </Link>
-            <Link
-              className="my-1 text-sm font-medium text-white sm:text-xl sm:font-medium md:mx-4 md:my-0"
-              to="#"
-            >
-              Shop
-            </Link>
-            <Link
-              className="my-1 text-sm font-medium text-white sm:text-xl sm:font-medium md:mx-4 md:my-0"
-              to="#"
-            >
-              Contact
-            </Link>
-            <Link
-              className="my-1 text-sm font-medium text-white sm:text-xl sm:font-medium md:mx-4 md:my-0"
-              to="#"
-            >
-              About
-            </Link>
+            {navLinks.map(navLink => (
+              <Link
+                className="transition duration-300 ease-linear my-1 text-sm font-medium text-white border-b-2 border-transparent hover:border-secondary sm:text-xl sm:font-medium md:mx-4 md:my-0"
+                to={navLink.endPoint}
+              >
+                {navLink.title}
+              </Link>
+            ))}
           </div>
         </div>
       </div>

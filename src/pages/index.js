3b5fe07e1eecdb606561b6892c56ffee92a946ee
Anandhan <anandhanedom@gmail.com>
2021-06-06 05:20:02 +0530

    fix:sadasdadads

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero.jsx";
+import WhoWeAre from "../components/WhoWeAre.jsx";
 
 const Home = () => {
   return (
     <div className="font-body">
       <Hero />
+      <WhoWeAre />
     </div>
   );
 };

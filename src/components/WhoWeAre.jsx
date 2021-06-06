4a1eb111a01e1467957b208dc386cb28c3b6f7c0
Anandhan <anandhanedom@gmail.com>
2021-06-06 08:14:19 +0530

    fix: padddingsadsd

--- src/components/WhoWeAre.jsx
+++ src/components/WhoWeAre.jsx
@@ -3,7 +3,7 @@ import Container from "../components/utils/Container";
 
 const WhoWeAre = () => {
   return (
-    <section className="text-alpha bg-white py-16 sm:py-20 md:py-32 lg:py-40">
+    <section className="text-alpha bg-white py-20 lg:py-40">
       <Container>
         <h2 className="max-w-xl text-4xl sm:text-5xl">
           We’re a team of humans with the strategy, tools, and talent to provide
